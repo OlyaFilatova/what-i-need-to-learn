b9034a1cc3518588e17460626ada8016b6e3a79f
Olga Filatova <olyafilatov@gmail.com>
2019-11-25 11:39:51 +0200

    renamed use case

--- src/policy/use-cases/parse-words-from-text.spec.ts
+++ src/policy/use-cases/parse-words-from-text.spec.ts
@@ -1,45 +1,44 @@
-import {ParseText} from './parse-text';
-import {WordCount} from '../entities/word-count';
+import {ParseWordsFromText} from './parse-words-from-text';
 
-function createParseText(): ParseText {
-  return new ParseText();
+function createParseText(): ParseWordsFromText {
+  return new ParseWordsFromText();
 }
 
 describe('Parse Text', () => {
   it('should return empty list on empty string passed', () => {
-    const parseText: ParseText = createParseText();
+    const parseText: ParseWordsFromText = createParseText();
     expect(parseText.parse('')).toEqual([]);
   });
   it('should return list with one WordCount on string when one word passed', () => {
-    const parseText: ParseText = createParseText();
+    const parseText: ParseWordsFromText = createParseText();
     const res = parseText.parse('sea');
     expect(res.length).toBe(1);
     expect(res[0].word.text).toBe('sea');
     expect(res[0].count).toBe(1);
   });
   it('should return list with two WordCount when two words passed', () => {
-    const parseText: ParseText = createParseText();
+    const parseText: ParseWordsFromText = createParseText();
     const res = parseText.parse('sea salt');
     expect(res.length).toBe(2);
     expect(res[0].word.text).toBe('sea');
     expect(res[1].word.text).toBe('salt');
   });
   it('should return list with one WordCount when one word typed twice passed', () => {
-    const parseText: ParseText = createParseText();
+    const parseText: ParseWordsFromText = createParseText();
     const res = parseText.parse('buffalo buffalo');
     expect(res.length).toBe(1);
     expect(res[0].word.text).toBe('buffalo');
     expect(res[0].count).toBe(2);
   });
   it('should return list with one WordCount with count 3 when one word typed three times passed', () => {
-    const parseText: ParseText = createParseText();
+    const parseText: ParseWordsFromText = createParseText();
     const res = parseText.parse('police police police');
     expect(res.length).toBe(1);
     expect(res[0].word.text).toBe('police');
     expect(res[0].count).toBe(3);
   });
   it('should return list with two WordCount where word "second" goes first when "test second second" passed', () => {
-    const parseText: ParseText = createParseText();
+    const parseText: ParseWordsFromText = createParseText();
     const res = parseText.parse('test second second');
     expect(res.length).toBe(2);
     expect(res[0].word.text).toBe('second');
