c6e25dac87de946c8f2c28d68970bf52d3d5a20f
Olga Filatova <olyafilatov@gmail.com>
2022-11-06 02:58:11 +0200

    Decouple data management

--- src/app/services/word-storage.service.ts
+++ src/app/services/word-storage.service.ts
@@ -1,17 +1,14 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 
-import { BaseStorageService } from './base-storage.service';
-
 import { WordInput } from '../input/word.input';
+import { WordStorage } from 'wntl-storage';
 
 @Injectable({
     providedIn: 'root',
 })
-export class WordStorageService extends BaseStorageService<WordInput> {
+export class WordStorageService extends WordStorage<WordInput> {
     constructor(protected firestore: AngularFirestore) {
         super(firestore);
-
-        this.collectionName = 'known-words';
     }
 }
